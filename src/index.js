825ad9f4b12f10f4cff1221f22f1b9db82fe65da
Choulak Ahmed <ahmedchoulak2004@gmail.com>
2024-07-08 17:09:45 +0100

    updt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
-import { getWeatherData } from './js/api';
 import { showError, showCurrentMeteo } from './js/dom';
 
 // Récupérer la ville recherchée par l'utilisateur
@@ -28,6 +27,24 @@ form.addEventListener('submit', async (event) => {
     }
     form.reset();
 });
+
+
+async function getWeatherData(location) {
+    const apiKey = 'e9497b296f0a4448b75164057240407';
+    const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3`;
+    const response = await fetch(apiUrl);
+    console.log("Response status: ", response.status); // Log le statut de la réponse
+
+    if (!response.ok) {
+        if (response.status === 400) {
+            throw new Error("City not found");
+        }
+        throw new Error("Couldn't fetch the data from the API");
+    }
+    const data = await response.json();
+    console.log("Weather data: ", data); // Log les données retournées
+    return data;
+}
 
 
 function showWeatherData(data) {
